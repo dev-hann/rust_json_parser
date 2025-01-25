c09967914393a0c5edd02ca24488fd1499225941
hann <developer.hann@gmail.com>
2025-01-25 21:51:18 +0900

    🎨 Feat: result view

--- src/home_view.rs
+++ src/home_view.rs
@@ -1,40 +1,45 @@
-use eframe::{egui, App};
+use eframe::{egui::{self}, App};
 
 pub struct HomeView {
-    json_input: String,
+    input: String,
+    output:String,
 }
 
 impl Default for HomeView {
     fn default() -> Self {
         return Self {
-            json_input: "".to_string(),
+            input: r#"{"1234":"1234","a":{"b":{"c":"d"}}}"#.to_string(),
+            output: "".to_string(),
         };
     }
 }
 
 impl App for HomeView {
     fn update(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
-
+        let input = serde_json::from_str::<serde_json::Value>(&self.input);
+        self.output = serde_json::to_string_pretty(&input.unwrap()).unwrap();
+        
         egui::CentralPanel::default().show(ctx, |ui| {
         let available_size = ui.available_size();
         let width = available_size.x * 0.5;
-        let height = available_size.y;
+        let height = available_size.y*0.9;
             ui.heading("Json Parser");
-            ui.horizontal(|ui| {
+            ui.horizontal_top(|ui| {
                 ui.allocate_ui(egui::Vec2::new(width, height), |ui| {
                     ui.vertical(|ui| {
                         ui.label("Json Input");
-                        ui.add_sized([width, height], egui::TextEdit::multiline(&mut self.json_input));
+                        ui.add_sized([width, height], egui::TextEdit::multiline(&mut self.input));
                     });
                 });
                 ui.allocate_space(egui::Vec2::new(16.0, 16.0));
                 ui.allocate_ui(egui::Vec2::new(width, height), |ui| {
                     ui.vertical(|ui| {
                         ui.label("Json Output");
-                        ui.selectable_label(false, self.json_input.clone());
+                        ui.add_sized([width, height], egui::TextEdit::multiline(&mut self.output).interactive(false));
                     });
                 });
             });
         });
     }
 }
+
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,9 @@ mod home_view;
 use home_view::HomeView;
 
 fn main() -> eframe::Result {
-    let option = eframe::NativeOptions {
-        // viewport: eframe::egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
-        ..Default::default()
-    };
     return eframe::run_native(
         "JsonParser",
-        option,
+        eframe::NativeOptions::default(),
         Box::new(|_| Ok(Box::<HomeView>::default())),
     );
 }
